005736f7df814b73d17908783e6d418a0b275974
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2023-06-10 19:50:15 +0530

    Fix bug to download image correctly and remove identifying details

--- IdentificationModel/GoogleAPIImageScraper.py
+++ IdentificationModel/GoogleAPIImageScraper.py
@@ -3,5 +3,4 @@ from google_images_search import GoogleImagesSearch
 gis = GoogleImagesSearch('', '')
 query = input("Enter your search query: ")
 numberOfResults = int(input("Enter the number of images you want to download: "))
-gis.search(search_params = {'q': query, 'num': numberOfResults, 'fileType': 'jpg'} , path_to_dir='/images/', custom_image_name = query)
-gis.results()
+gis.search(search_params = {'q': query, 'num': numberOfResults, 'fileType': 'jpg'} , path_to_dir = "", custom_image_name = query)
