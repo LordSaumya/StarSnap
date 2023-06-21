a0fcb218d8ab2f9beebbb30c5cf17ad170d22f12
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2023-06-21 17:41:46 +0530

    Revert files after lfs uninstall

--- starsnap/src/identificationTab.js
+++ starsnap/src/identificationTab.js
@@ -1,3 +1,51 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a429ee999f9275e2fb594bea5eb109cafb0b65143b76e9fe197c7bdd676e3ad9
-size 1287
+//Imports
+import React from 'react';
+import { useState } from 'react';
+import Navbar from './navbar.js';
+import LazyLoad from 'react-lazyload';
+import moment from 'moment';
+import background from './images/background.jpg';
+import { FaArrowCircleUp } from 'react-icons/fa';
+import moderatorList from './moderatorsList.json';
+import Post from './post.js';
+import {
+    Box,
+    Text,
+    Link,
+    Badge,
+    useColorMode,
+    Select,
+    Button,
+    Container,
+    Heading,
+    Divider,
+    FormControl,
+    FormLabel,
+    Spinner,
+    FormErrorMessage,
+    FormHelperText,
+    Collapse,
+    Flex,
+    Input,
+} from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { CheckIcon, WarningIcon, AddIcon, ChatIcon, TriangleDownIcon, TriangleUpIcon, Search2Icon } from '@chakra-ui/icons';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+//Body
+
+export default function Home() {
+    const { colorMode, toggleColorMode } = useColorMode();
+    return (
+    <Box>
+        <Navbar />
+        <Flex justifyContent = "center" height = "100vh" width = "100vw" bgImage={background}>
+            <Box minHeight = "100vh" width="70%" bg = {colorMode !== 'dark' ? 'white' : 'gray.800'} py = "1em">
+
+                
+
+            </Box>
+        </Flex>
+    </Box>
+    );
+}
--- starsnap/src/post.js
+++ starsnap/src/post.js
@@ -1,3 +1,81 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8307939d50f421d6312e6d29ce4e66332b0e96f498978efe1c8e63a65f76e0f1
-size 3213
+//Imports
+import React from 'react';
+import LazyLoad from 'react-lazyload';
+import moment from 'moment';
+import moderatorList from './moderatorsList.json';
+import UseFetch from './UseFetch.js';
+import { FaLink, FaComment, FaThumbsUp} from 'react-icons/fa';
+import cameraIcon from './images/cameraIcon.png';
+import testImage from './images/testImage.jpg';
+import {
+    Box,
+    Text,
+    IconButton,
+    Link,
+    Badge,
+    Select,
+    Button,
+    Container,
+    HStack,
+    Heading,
+    Divider,
+    Collapse,
+    Image,
+    ButtonGroup,
+} from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { CheckIcon, WarningIcon, AddIcon, ChatIcon, TriangleDownIcon, TriangleUpIcon, Search2Icon } from '@chakra-ui/icons';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export default function Post(props) {
+    //props: PostID, image, caption, date, UserID, Tag
+
+    const PostID = props.PostID;
+    const image = props.Image;
+    const Caption = props.Caption;
+    const Date = props.Date;
+    const isLiked = false;
+    const UserID = props.UserID;
+    const Tag = props.Tag;
+
+    const Navigate = useNavigate();
+    const timeAgo = moment(Date).fromNow();
+    const author = UseFetch("localhost:4000/" + props.UserID); //REPLACE URL
+    const authorUsername = author ? "" : author.username;
+    
+
+    const mods = JSON.parse(JSON.stringify(moderatorList)).moderators;
+    const isMod = mods.includes(author.username);
+
+    return (
+        <LazyLoad>
+            <Container boxShadow = "md" minWidth = "70%" padding = "10px" name = "postContainer" marginBottom = "10px">
+                <Badge ml='1' colorScheme='green' float="right" name="postTag">
+                    {Tag}
+                </Badge>
+                {isMod ?
+                    <Badge ml='1' colorScheme='yellow' float="right" name="mod">
+                        Moderator Post
+                    </Badge>
+                    : <></>
+                }
+                    <HStack><Image  src={cameraIcon} height = "2em" alt="ProfPic" borderRadius={10000} _hover={{border: "1px"}} /><Text fontSize="sm" color="gray.500"><Link href={"/ProfilePage/" + authorUsername} color="teal.500" >{authorUsername}</Link> {timeAgo}</Text></HStack>
+                    <Divider color = "gray.500" paddingTop = "5px" />
+                    <Image height = "40vh" width = "100%" py = "8px" src={image} alt="PostImage" />
+                    <Divider color = "gray.500" paddingTop = "5px" />
+                <HStack height = "5em">
+                    <ButtonGroup isAttached variant="outline" borderRight = "5px">
+                <IconButton onMouseDown = {LikePost(PostID, UserID)} color = {isLiked ? "blue.500" : ""} size='md' mt = '1rem' icon = {<FaThumbsUp />} />
+                <IconButton size='md' mt = '1rem' icon = {<FaComment />} />
+                <IconButton size='md' mt = '1rem' icon = {<FaLink />} />
+                </ButtonGroup>
+                <Text paddingLeft = "1em" paddingTop = "1em" fontSize="sm" color="gray.500" name="postCaption">
+                    {Caption}
+                </Text>
+                </HStack>
+            </Container>
+        </LazyLoad>
+    );
+}
+
+function LikePost(PostID, UserID) {}
